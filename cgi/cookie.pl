fbff1dd9fa49bfb18d6f0000263457ab9183e4a0
root <root@apps.(none)>
2017-04-04 18:52:17 +0800

    Delete

--- cgi/cookie.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-$username="Tim";
-$usergroup="admin";
-print "Set-Cookie:UserID=$username;\n";
-print "Set-Cookie:UserGroup=$usergroup;\n";
-print "Set-Cookie:Path=/;\n";
-print "Content-type:text/html\n\n";
-print "Set cookie finished";
